b610e3bcaad10a8258c266d474b509655ef73b30
Mert Özdemir <mertozdemir@192.168.1.105>
2024-08-06 23:41:28 +0300

    node rate limiter application

---
+++ src/config/server_config.js
@@ -0,0 +1,8 @@
+// Server configuration
+var dotenv = require("dotenv");
+
+function initServerConfig () {
+    dotenv.config();
+};
+
+module.exports = initServerConfig;
---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+// Express Router
+const express = require("express");
+const router = express.Router();
+
+// Router functions
+const {loginRateLimiter, registerRateLimiter} = require("../middlewares/rate_limit/rateLimiters");
+
+// Server login route and route functions -> http://localhost:8080/api/login
+router.get('/login', loginRateLimiter, async(req,res)=> {
+    try{
+      res.write("Login Page");
+      res.end();
+    }
+    catch(err){}
+});
+
+// Server register route and route functions -> http://localhost:8080/api/register
+router.get('/register', registerRateLimiter, async (req,res)=>{
+    try{
+        res.write("Register Page");
+        res.end();
+    }
+    catch(err){}
+});
+
+module.exports = router;
